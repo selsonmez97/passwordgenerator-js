1d94f67fd61a046c86a4bf2d4a65069fcd881dff
selinasonmez <selinasonmez97@gmail.com>
2023-07-11 11:29:40 +0300

    Changed length of password characters.

--- index.js
+++ index.js
@@ -4,18 +4,17 @@ const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O"
 function generatePassword() {
     const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
   
-    // Generate random passwords
+
     let password1 = generateRandomPassword(characters);
     let password2 = generateRandomPassword(characters);
   
-    // Display passwords in the input fields
     document.getElementById("inputField1").value = password1;
     document.getElementById("inputField2").value = password2;
   }
   
   function generateRandomPassword(characters) {
     let password = "";
-    const passwordLength = 8; // Adjust the password length as desired
+    const passwordLength = 15;
   
     for (let i = 0; i < passwordLength; i++) {
       let randomIndex = Math.floor(Math.random() * characters.length);
